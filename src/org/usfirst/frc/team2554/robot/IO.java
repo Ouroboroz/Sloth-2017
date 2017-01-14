919ad7ac28238ff516feedb47c85eecc11808046
Creepermania9 <KevinYang@Haos-MBP.home>
2017-01-14 16:03:09 -0500

    IO+Vision
    Added class that will list all of the IO ports and numbers.
    Began working on Vision.

---
+++ src/org/usfirst/frc/team2554/robot/IO.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team2554.robot;
+
+public class IO {
+	//Front Left, Back Left, Front Right, Back Right
+	static int driveTrain[] = {0,1,3,4};
+	
+	}
---
+++ src/org/usfirst/frc/team2554/robot/Vision.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team2554.robot;
+import org.opencv.core.*;
+import org.opencv.imgproc.*;
+import edu.wpi.cscore.*;
+public class Vision {
+	UsbCamera visionCam = new UsbCamera("Main Vision Camera", 1);
+}
