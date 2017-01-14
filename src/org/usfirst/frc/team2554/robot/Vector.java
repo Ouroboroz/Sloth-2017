28f67fcde180c7fd03a6ff062ac6ee08c8fd573d
Creepermania9 <creepermania9@live.com>
2017-01-13 23:28:10 -0500

    Added Vector Class

--- src/org/usfirst/frc/team2554/robot/Vector.java
+++ src/org/usfirst/frc/team2554/robot/Vector.java
@@ -1,12 +1,38 @@
 package org.usfirst.frc.team2554.robot;
-
-/**
- * Created by Arjun on 1/13/2017.
- */
+import java.lang.Math;
 public class Vector {
-
-
-    public Vector( double magnitude, double direction ) {
-
+	private double x, y, magnitude, direction;
+    public Vector(double magnitude, double direction, boolean isNorth) {
+    	x = magnitude*Math.sin(direction/180.0*Math.PI);
+    	y = magnitude*Math.cos(direction/180.0*Math.PI);
+    	this.magnitude = magnitude;
+    	this.direction = direction;
+    }
+    public Vector(double xVal, double yVal){
+    	x = xVal;
+    	y = yVal;
+    	magnitude = Math.sqrt(x*x+y*y);
+    	direction = Math.atan(x/y)*180.0/Math.PI;
+    }
+    
+    public Vector add(Vector nVector){
+    	return new Vector(x + nVector.returnVal(1),y + nVector.returnVal(2));
+    }
+    public Vector subtract(Vector nVector){
+    	return new Vector(x - nVector.returnVal(1),y - nVector.returnVal(2));
+    }
+    public double returnVal(int num){
+    	switch(num){
+    	case 1:
+    		return x;
+    	case 2:
+    		return y;
+    	case 3:
+    		return magnitude;
+    	case 4:
+    		return direction;
+    	default:
+    		return 0.0;
+    	}
     }
 }
