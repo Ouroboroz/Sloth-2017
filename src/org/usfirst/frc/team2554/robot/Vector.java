0828cceea35558e45cbcb9d9e114764e3e21b9c3
Arjun Srivastav <arjun.stv@gmail.com>
2017-01-13 23:12:39 -0500

    clean up clean up everybody do your share

---
+++ src/org/usfirst/frc/team2554/robot/Vector.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team2554.robot;
+
+/**
+ * Created by Arjun on 1/13/2017.
+ */
+public class Vector {
+
+
+    public Vector( double magnitude, double direction ) {
+
+    }
+}
