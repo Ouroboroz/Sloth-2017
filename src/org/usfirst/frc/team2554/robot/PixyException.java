63d35d774efd99c771435059160c921606c5a9f9
Creepermania9 <creepermania9@live.com>
2017-01-16 23:29:58 -0500

    PixyCam Support
    Added class to read from PixyCam

---
+++ src/org/usfirst/frc/team2554/robot/PixyException.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team2554.robot;
+
+public class PixyException extends Exception{
+	public PixyException(String message){
+		super(message);
+	}
+}
---
+++ src/org/usfirst/frc/team2554/robot/PixyPacket.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team2554.robot;
+
+public class PixyPacket {
+	public int X;
+	public int Y;
+	public int Width;
+	public int Height;
+}
